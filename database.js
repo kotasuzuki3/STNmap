affa2784c95f1b8f06c0a14697fc6c23ea63235e
kotasuzuki3 <kota.suzuki2021@gmail.com>
2023-06-26 17:57:11 -0500

    database now connected to postgre

--- database.js
+++ database.js
@@ -1,18 +1,13 @@
-const {Client} = require('pg')
+const { Client } = require('pg')
 const client = new Client({
-    host: "ls-8ed9bf80f7fa0bbc9ef8a691ff65762675ab3c1a.c5eity8epojm.us-east-2.rds.amazonaws.com",
-    user: "nonadmin",
-    port: 5432,
-    password: "z,^;ojQk]YH3Hmau~bk#<2(*CGW0k[Ed",
-    database: "stn-staging"
+  user: 'nonadmin',
+  host: 'ls-8ed9bf80f7fa0bbc9ef8a691ff65762675ab3c1a.c5eity8epojm.us-east-2.rds.amazonaws.com',
+  database: 'dbstn',
+  password: 'z,^;ojQk]YH3Hmau~bk#<2(*CGW0k[Ed',
+  port: 5432,
 })
 
-client.connect()
-/* client.query(`Select * from user`, (err, res) => {
-    if (!err){
-        console.log(res.rows);
-    } else {
-        console.log(err.message);
-    }
-    client.end;
-}) */
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
