b33ee0f57bd689d0a9ecb6b564e4ecd8d628b313
kotasuzuki3 <kota.suzuki2021@gmail.com>
2023-07-08 15:31:57 -0500

    heat map connected with SQL and general clean-up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import Map from "./Map_Dark";
+import Map from "./Map";
 
 export default function App() {
   return (
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "leaflet.heat";
+import "leaflet/dist/leaflet.css";
 import "./index.css";
 
 export default function Map() {
@@ -9,106 +10,143 @@ export default function Map() {
   const heatLayerRef = useRef(null);
   const timeSliderRef = useRef(null);
   const timeLabelRef = useRef(null);
+  const [heatmapData, setHeatmapData] = useState([]);
+  const [dateRange, setDateRange] = useState({ minDate: new Date(), maxDate: new Date() });
+
+  const filterValidData = (data) => {
+    const validData = data.filter((point) => {
+      return point.latitude !== null && point.longitude !== null;
+    }).map((point) => ({
+      ...point, 
+      incident_date: new Date(point.incident_date).toISOString().split('T')[0],
+    }));
+  
+    console.log("Valid Data:", validData);
+  
+    return validData;
+  };
+  
+  const updateHeatmap = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/data');
+      const jsonData = await response.json();
+      const validData = filterValidData(jsonData);
+      setHeatmapData(validData);
+  
+      // Clear previous heatmap layer
+      if (heatLayerRef.current) {
+        heatLayerRef.current.remove();
+      }
+  
+      // Create a new Leaflet Heatmap layer
+      const heatLayer = L.heatLayer(
+        validData.map((dataPoint) => [dataPoint.latitude, dataPoint.longitude])
+      );
+  
+      // Add the new heatmap layer to the map
+      heatLayer.addTo(mapRef.current);
+  
+      // Store the reference to the new heatmap layer for future removal
+      heatLayerRef.current = heatLayer;
+  
+      // Update the date range
+      const dates = validData.map((point) => new Date(point.incident_date));
+      const minDate = new Date(Math.min(...dates));
+      const maxDate = new Date(Math.max(...dates));
+      setDateRange({ minDate, maxDate });
+
+      // Get the selected date from the timeline slider
+      const timelineValue = timeSliderRef.current.value;
+      const currentTime = new Date(
+      minDate.getTime() + (maxDate.getTime() - minDate.getTime()) * (timelineValue / 100)
+      );
+        
+      // Update the time label with the selected date
+      const formattedDate = currentTime.toLocaleDateString();
+      timeLabelRef.current.textContent = formattedDate;
+    
+  
+      // Filter the heatmap data based on the current time
+      const filteredData = validData.filter(
+        (dataPoint) => new Date(dataPoint.incident_date) <= currentTime
+      );
+  
+      // Update the heatmap layer with the filtered data
+      const heatPoints = filteredData.map((point) => [
+        parseFloat(point.latitude),
+        parseFloat(point.longitude),
+      ]);
+      heatLayerRef.current.setLatLngs(heatPoints);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+
+
 
   useEffect(() => {
-    const map = L.map(mapRef.current).setView([37.0902, -95.7129], 4);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    let map = null;
 
-    const heatLayer = L.heatLayer([], { radius: 20 });
-    heatLayer.addTo(map);
-
-    mapRef.current = map;
-    heatLayerRef.current = heatLayer;
-  }, []);
-
-  useEffect(() => {
-    const heatLayer = heatLayerRef.current;
-    const timeSlider = timeSliderRef.current;
-    const timeLabel = timeLabelRef.current;
-
-    const fetchData = async () => {
+    const initializeMap = async () => {
       try {
-        const response = await fetch("/api/data");
-        const data = await response.json();
-
-        const heatPoints = data.map(item => [item.latitude, item.longitude]);
-        heatLayer.setLatLngs(heatPoints);
-
-        // Extract incident dates
-        const incidentDates = data.map(item => new Date(item.incident_date));
-
-        // Find the min and max incident dates
-        const minDate = new Date(Math.min(...incidentDates));
-        const maxDate = new Date(Math.max(...incidentDates));
-
-        // Update the time slider properties
-        timeSlider.min = minDate.getTime();
-        timeSlider.max = maxDate.getTime();
-        timeSlider.value = minDate.getTime();
-
-        // Update the time label with the initial date
-        const formattedDate = minDate.toLocaleDateString();
-        timeLabel.textContent = formattedDate;
-
+        const response = await fetch("http://localhost:3001/api/data");
+        const jsonData = await response.json();
+        const validData = filterValidData(jsonData);
+        setHeatmapData(validData);
+    
+        map = L.map(mapRef.current, {
+          zoomControl: false,
+        }).setView([37.0902, -95.7129], 4);
+    
+        L.tileLayer(
+          "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+          {
+            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+            maxZoom: 16,
+          }
+        ).addTo(map);
+    
+        const heatLayer = L.heatLayer([], {
+          radius: 25,
+          blur: 15,
+        });
+        heatLayer.addTo(map);
+        
+    
+        heatLayerRef.current = heatLayer;
+        mapRef.current = map;
+    
+        const logoContainer = L.DomUtil.create("div", "logo-container");
+        const logoImage = document.createElement("img");
+        logoImage.src =
+          "https://www.nonopera.org/WP2/wp-content/uploads/2016/12/NONopNEWlogo-round300-1.jpg";
+        logoContainer.appendChild(logoImage);
+        map.getContainer().appendChild(logoContainer);
+    
+        timeSliderRef.current.addEventListener("input", updateHeatmap);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error initializing map:", error);
       }
     };
+    
+    
 
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const map = mapRef.current;
-    const timeSliderContainer = document.querySelector(".slider-container");
-
-    const updateSliderPosition = () => {
-      const mapBounds = map.getContainer().getBoundingClientRect();
-      timeSliderContainer.style.top = `${mapBounds.top + 10}px`;
-      timeSliderContainer.style.left = `${mapBounds.left}px`;
-    };
-
-    const updateSliderOnMove = () => {
-      updateSliderPosition();
-    };
-
-    map.on("zoomend", updateSliderPosition);
-    map.on("move", updateSliderOnMove);
-
-    updateSliderPosition();
-
-    const zoomControlContainer = L.control.zoom({ position: "topleft" }).addTo(map);
-    const zoomControlElement = zoomControlContainer.getContainer();
-
-    const zoomInButton = zoomControlElement.getElementsByClassName("leaflet-control-zoom-in")[0];
-    const zoomOutButton = zoomControlElement.getElementsByClassName("leaflet-control-zoom-out")[0];
-
-    zoomInButton.innerHTML = "+";
-    zoomOutButton.innerHTML = "-";
-
-    const customZoomControls = document.createElement("div");
-    customZoomControls.classList.add("custom-zoom-controls");
-    customZoomControls.appendChild(zoomInButton);
-    customZoomControls.appendChild(zoomOutButton);
-    map.getContainer().appendChild(customZoomControls);
+    initializeMap();
 
     return () => {
-      map.off("zoomend", updateSliderPosition);
-      map.off("move", updateSliderOnMove);
-      zoomControlContainer.remove();
+      const timeSlider = timeSliderRef.current;
+      timeSlider.removeEventListener("input", updateHeatmap);
+
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
     };
   }, []);
 
-  const handleSliderChange = () => {
-    const timeSlider = timeSliderRef.current;
-    const timeLabel = timeLabelRef.current;
-
-    const currentTime = parseInt(timeSlider.value);
-    const formattedDate = new Date(currentTime).toLocaleDateString();
-    timeLabel.textContent = formattedDate;
+  const handleResetZoom = () => {
+    const map = mapRef.current;
+    map.setView([37.0902, -95.7129], 4);
   };
 
   return (
@@ -123,14 +161,19 @@ export default function Map() {
           id="timeSlider"
           type="range"
           min="0"
-          max="1"
+          max="100"
           step="1"
           defaultValue="0"
           className="slider"
-          onChange={handleSliderChange}
+          onChange={updateHeatmap}
         />
         <label ref={timeLabelRef} id="timeLabel" className="time-label"></label>
       </div>
+      <button className="reset-zoom-button" onClick={handleResetZoom}>
+        Reset Zoom
+      </button>
     </div>
   );
 }
+
+
