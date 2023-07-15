956327102ae111e52b937514df543af4298a0384
kotasuzuki3 <kota.suzuki2021@gmail.com>
2023-07-15 14:35:09 -0500

    more refinement of dashboard

--- src/Map_Dashboard.jsx
+++ src/Map_Dashboard.jsx
@@ -10,6 +10,7 @@ export default function Map() {
   const timeSliderRef = useRef(null);
   const timeLabelRef = useRef(null);
   const [showMethodology, setShowMethodology] = useState(false);
+  const [collapseDate, setCollapseDate] = useState(null);
   const [showAbout, setShowAbout] = useState(false);
   const [showDashboard, setShowDashboard] = useState(true); // State to control the dashboard visibility
   const [showDashboardContent, setShowDashboardContent] = useState(true);
@@ -26,8 +27,11 @@ export default function Map() {
   };
 
   const toggleDashboard = () => {
+    if (showDashboard) {
+      setCollapseDate(new Date()); // Set the collapse date when collapsing the dashboard
+    }
     setShowDashboard(!showDashboard);
-    setDashboardVisible(!showDashboardContent);
+    setDashboardVisible(!dashboardVisible);
   };
 
   const filterValidData = (data) => {
@@ -152,12 +156,6 @@ export default function Map() {
         mapRef.current = map;
         heatLayerRef.current = heatLayer;
 
-        const logoContainer = L.DomUtil.create("div", "logo-container");
-        const logoImage = document.createElement("img");
-        logoImage.src = "https://www.nonopera.org/WP2/wp-content/uploads/2016/12/NONopNEWlogo-round300-1.jpg";
-        logoContainer.appendChild(logoImage);
-        map.getContainer().appendChild(logoContainer);
-
         timeSliderRef.current.addEventListener("input", updateHeatmap);
       } catch (error) {
         console.error("Error initializing map:", error);
@@ -186,124 +184,133 @@ export default function Map() {
     <div className="map-container">
       <div ref={mapRef} className="map"></div>
       {showDashboard && (
-  <div className="dashboard">
-    <div className="dashboard-header">
-      <div className="dashboard-title"></div>
-      {dashboardVisible ? (
-        <img
-          src="https://cdn3.iconfinder.com/data/icons/arrows-219/24/collapse-left-512.png"
-          alt="Collapse"
-          className="dashboard-icon"
-          onClick={toggleDashboard}
-          style={{ width: "25px", height: "25px" }}
-        />
-      ) : (
-        <>
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-256/free-collapse-right-1485695-1258916.png?f=webp"
-            alt="Reopen"
-            className="dashboard-icon"
-            onClick={toggleDashboard}
-            style={{ width: "25px", height: "25px" }}
-          />
-          <button className="dashboard-reopen" onClick={toggleDashboard}>
-            Reopen
-          </button>
-        </>
-      )}
-    </div>
-          {showDashboardContent && (
-          <div className="dashboard-content">
-            <div className="dashboard-section">
-              <div className="dashboard-section-title"></div>
-              <div className="dashboard-section-content">
-                <img
-                  src="https://cdn.icon-icons.com/icons2/1863/PNG/512/zoom-out-map_118446.png"
-                  alt="Reset Zoom"
-                  className="dashboard-icon"
-                  onClick={handleResetZoom}
-                  style={{ width: "25px", height: "25px", marginLeft: "-200px", marginTop: "75px"}}
-                />
-              </div>
-            </div>
-            <div className="dashboard-section">
-              <div className="dashboard-section-title"></div>
-              <div className="dashboard-section-content">
-                <img
-                  src="https://www.iconpacks.net/icons/1/free-information-icon-348-thumb.png"
-                  alt="About"
-                  className="dashboard-icon"
-                  onClick={toggleAbout}
-                  style={{ width: "30px", height: "30px", marginLeft: "-200px", marginTop: "5px"}}
-                />
-              </div>
-            </div>
-            <div className="dashboard-section">
-              <div className="dashboard-section-title"></div>
-              <div className="dashboard-section-content">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/6345/6345343.png"
-                  alt="Methodology"
-                  className="dashboard-icon"
-                  onClick={toggleMethodology}
-                  style={{ width: "38px", height: "38px", marginLeft: "-195px", marginTop: "0px"}}
-                />
-              </div>
-            </div>
-            <div className="dashboard-section">
-              <div className="dashboard-section-title">Time Slider</div>
-              <div className="dashboard-section-content">
-                <input
-                  type="range"
-                  min="0"
-                  max="100"
-                  step="1"
-                  defaultValue="0"
-                  className="time-slider"
-                  ref={timeSliderRef}
-                />
-              
-                <div className="time-label" ref={timeLabelRef}></div>
-              </div>
-            </div>
+        <div className={`dashboard ${showDashboard ? "" : "collapsed"}`}>
+          <div className="dashboard-header">
+            <div className="dashboard-title"></div>
+            <img
+              src={dashboardVisible ? "https://cdn3.iconfinder.com/data/icons/arrows-219/24/collapse-left-512.png" : "https://cdn.iconscout.com/icon/free/png-256/free-collapse-right-1485695-1258916.png?f=webp"}
+              alt={dashboardVisible ? "Collapse" : "Reopen"}
+              className="dashboard-icon"
+              onClick={toggleDashboard}
+              style={{ width: "25px", height: "25px" }}
+            />
           </div>
+          <div className="logo-container">
+            <img
+              src="https://www.nonopera.org/WP2/wp-content/uploads/2016/12/NONopNEWlogo-round300-1.jpg"
+              alt="Logo"
+              className="logo-image"
+            />
+          </div>
+          {showDashboardContent && (
+            <div className="dashboard-content">
+              <div className="dashboard-section">
+                <div className="dashboard-section-title"></div>
+                <div className="dashboard-section-content">
+                  <img
+                    src="https://cdn.icon-icons.com/icons2/1863/PNG/512/zoom-out-map_118446.png"
+                    alt="Reset Zoom"
+                    className="dashboard-icon"
+                    onClick={handleResetZoom}
+                    style={{ width: "25px", height: "25px", marginLeft: "-200px", marginTop: "75px" }}
+                  />
+                </div>
+              </div>
+              <div className="dashboard-section">
+                <div className="dashboard-section-title"></div>
+                <div className="dashboard-section-content">
+                  <img
+                    src="https://www.iconpacks.net/icons/1/free-information-icon-348-thumb.png"
+                    alt="About"
+                    className="dashboard-icon"
+                    onClick={toggleAbout}
+                    style={{ width: "30px", height: "30px", marginLeft: "-200px", marginTop: "5px" }}
+                  />
+                </div>
+              </div>
+              <div className="dashboard-section">
+                <div className="dashboard-section-title"></div>
+                <div className="dashboard-section-content">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/6345/6345343.png"
+                    alt="Methodology"
+                    className="dashboard-icon"
+                    onClick={toggleMethodology}
+                    style={{ width: "38px", height: "38px", marginLeft: "-195px", marginTop: "0px" }}
+                  />
+                </div>
+              </div>
+              <div className="dashboard-section">
+                <div className="dashboard-section-title">Time Slider</div>
+                <div className="dashboard-section-content">
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    step="1"
+                    defaultValue="0"
+                    className="time-slider"
+                    ref={timeSliderRef}
+                    onChange={updateHeatmap}
+                  />
+
+                  <div className="time-label" ref={timeLabelRef}></div>
+                </div>
+              </div>
+            </div>
           )}
         </div>
       )}
-      {showAbout && (
-        <div className="popup"> {/* Updated class name */}
-        <div className="popup-content"> {/* Added a wrapper div for content */}
-          <p>
-            SAY THEIR NAMES is an ongoing research and mapping project, intended to identify and remember Black Americans
-            killed by police violence since 1919. It is designed with an open-ended timeline to permit several successive
-            years of research, the concurrent development of an interactive map, and the accumulation of community contributions.
-            This map is truly a participatory and interactive project. In the coming months and years as we continue to accumulate
-            research data and develop the online interactive map, we will add the capability to submit data through an online form.
-          </p>
-          <p>
-            For now, please email us if you have additional information on a currently listed Black American, or with additional
-            names that should be included in the map. You may also fill out a form on our website <a href="https://form.jotform.com/222938481763163">HERE</a>.
-          </p>
-          <p>
-            To find out more about SAY THEIR NAMES, including research methodology, current and past researchers, and how you can participate,
-            please visit the <a href="https://www.nonopera.org/WP2/voices/say-their-names/">SAY THEIR NAMES webpage</a>.
-          </p>
-          <p>
-            SAY THEIR NAMES was formally launched in January 2021, and currently is staffed by a Lead Researcher-Supervisor, Ronald Browne;
-            a Project Manager, Dr. Saba Ayman-Nolley; Northeastern Illinois University academic supervisor for map development, Ting Liu;
-            two Northeastern Illinois University interns, research assistant Nozanin (Noza) Farrukhzoda and map developer Robert (Rob) Strzok;
-            and two volunteer community Research Assistants, Omid Nolley and Safira Newton-Matza. In addition, NON:op’s creative director,
-            Christophe Preissing, is assisting with the map development.
-          </p>
-          <p>
-            For more information, please email us at <a href="mailto:stn@nonopera.org">stn@nonopera.org</a>.
-          </p>
+      {!showDashboard && (
+        <div className="dashboard collapsed">
+          <div className="dashboard-header">
+            <div className="dashboard-title"></div>
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-256/free-collapse-right-1485695-1258916.png?f=webp"
+              alt="Reopen"
+              className="dashboard-icon"
+              onClick={toggleDashboard}
+              style={{ width: "25px", height: "25px" }}
+            />
+          </div>
+          <div className="dashboard-content"></div>
         </div>
-      </div>
-    )}
+      )}
+      {showAbout && (
+        <div className="popup">
+          <div className="popup-content">
+            <p>
+              SAY THEIR NAMES is an ongoing research and mapping project, intended to identify and remember Black Americans
+              killed by police violence since 1919. It is designed with an open-ended timeline to permit several successive
+              years of research, the concurrent development of an interactive map, and the accumulation of community contributions.
+              This map is truly a participatory and interactive project. In the coming months and years as we continue to accumulate
+              research data and develop the online interactive map, we will add the capability to submit data through an online form.
+            </p>
+            <p>
+              For now, please email us if you have additional information on a currently listed Black American, or with additional
+              names that should be included in the map. You may also fill out a form on our website{" "}
+              <a href="https://form.jotform.com/222938481763163">HERE</a>.
+            </p>
+            <p>
+              To find out more about SAY THEIR NAMES, including research methodology, current and past researchers, and how you can participate,
+              please visit the <a href="https://www.nonopera.org/WP2/voices/say-their-names/">SAY THEIR NAMES webpage</a>.
+            </p>
+            <p>
+              SAY THEIR NAMES was formally launched in January 2021, and currently is staffed by a Lead Researcher-Supervisor, Ronald Browne;
+              a Project Manager, Dr. Saba Ayman-Nolley; Northeastern Illinois University academic supervisor for map development, Ting Liu;
+              two Northeastern Illinois University interns, research assistant Nozanin (Noza) Farrukhzoda and map developer Robert (Rob) Strzok;
+              and two volunteer community Research Assistants, Omid Nolley and Safira Newton-Matza. In addition, NON:op’s creative director,
+              Christophe Preissing, is assisting with the map development.
+            </p>
+            <p>
+              For more information, please email us at <a href="mailto:stn@nonopera.org">stn@nonopera.org</a>.
+            </p>
+          </div>
+        </div>
+      )}
       {showMethodology && (
-          <div className="popup"> {/* Updated class name */}
-          <div className="popup-content"> {/* Added a wrapper div for content */}
+        <div className="popup">
+          <div className="popup-content">
             <p>
               SAY THEIR NAMES documents incidents that likely would not have resulted in the death of white Americans given
               the same set of circumstances. Each reported case is examined against certain criteria as our focus for the
@@ -317,6 +324,13 @@ export default function Map() {
           </div>
         </div>
       )}
+      <img
+        src="https://cdn.iconscout.com/icon/free/png-256/free-collapse-right-1485695-1258916.png?f=webp"
+        alt="Reopen"
+        className="reopen-button"
+        onClick={toggleDashboard}
+        style={{ width: "25px", height: "25px", marginLeft: "-30px", marginTop: "0px"}}
+      />
     </div>
-  );  
+  );
 }
