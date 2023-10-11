280580c0790ed0c0b58c28bc3e9e04e6e0c77d2f
kotasuzuki3 <kota.suzuki2021@gmail.com>
2023-10-10 23:29:14 -0500

    adding more filters, refining pop-up, adding state movement

--- src/PointMap.jsx
+++ src/PointMap.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "./index.css";
-import InputRange from 'react-input-range';
 
 export default function PointMap() {
   const mapRef = useRef(null);
@@ -15,11 +14,65 @@ export default function PointMap() {
   const [showAbout, setShowAbout] = useState(false);
   const [showMethodology, setShowMethodology] = useState(false);
   const [activeButton, setActiveButton] = useState("");
-  const [selectedState, setSelectedState] = useState("All");
   const [map, setMap] = useState(null); 
+  const [selectedState, setSelectedState] = useState("All");
   const [selectedGender, setSelectedGender] = useState("All");
-  const [selectedAgeRange, setSelectedAgeRange] = useState([0, 100]); 
+  const [selectedAgeRange, setSelectedAgeRange] = useState([0, 100]);
+ 
+  
 
+  const stateCoordinates = {
+    AL: { lat: 32.806671, lon: -86.791130 },
+    AK: { lat: 61.370716, lon: -152.404419 },
+    AZ: { lat: 33.729759, lon: -111.431221 },
+    AR: { lat: 34.969704, lon: -92.373123 },
+    CA: { lat: 36.116203, lon: -119.681564 },
+    CO: { lat: 39.059811, lon: -105.311104 },
+    CT: { lat: 41.597782, lon: -72.755371 },
+    DE: { lat: 39.318523, lon: -75.507141 },
+    FL: { lat: 27.766279, lon: -81.686783 },
+    GA: { lat: 33.040619, lon: -83.643074 },
+    HI: { lat: 21.094318, lon: -157.498337 },
+    ID: { lat: 44.240459, lon: -114.478828 },
+    IL: { lat: 40.349457, lon: -88.986137 },
+    IN: { lat: 39.849426, lon: -86.258278 },
+    IA: { lat: 42.011539, lon: -93.210526 },
+    KS: { lat: 38.526600, lon: -96.726486 },
+    KY: { lat: 37.668140, lon: -84.670067 },
+    LA: { lat: 31.169546, lon: -91.867805 },
+    ME: { lat: 44.693947, lon: -69.381927 },
+    MD: { lat: 39.063946, lon: -76.802101 },
+    MA: { lat: 42.230171, lon: -71.530106 },
+    MI: { lat: 43.326618, lon: -84.536095 },
+    MN: { lat: 45.694454, lon: -93.900192 },
+    MS: { lat: 32.741646, lon: -89.678696 },
+    MO: { lat: 38.456085, lon: -92.288368 },
+    MT: { lat: 46.921925, lon: -110.454353 },
+    NE: { lat: 41.125370, lon: -98.268082 },
+    NV: { lat: 38.313515, lon: -117.055374 },
+    NH: { lat: 43.452492, lon: -71.563896 },
+    NJ: { lat: 40.298904, lon: -74.521011 },
+    NM: { lat: 34.840515, lon: -106.248482 },
+    NY: { lat: 42.165726, lon: -74.948051 },
+    NC: { lat: 35.630066, lon: -79.806419 },
+    ND: { lat: 47.528912, lon: -99.784012 },
+    OH: { lat: 40.388783, lon: -82.764915 },
+    OK: { lat: 35.565342, lon: -96.928917 },
+    OR: { lat: 44.572021, lon: -122.070938 },
+    PA: { lat: 40.590752, lon: -77.209755 },
+    RI: { lat: 41.680893, lon: -71.511780 },
+    SC: { lat: 33.856892, lon: -80.945007 },
+    SD: { lat: 44.299782, lon: -99.438828 },
+    TN: { lat: 35.747845, lon: -86.692345 },
+    TX: { lat: 31.054487, lon: -97.563461 },
+    UT: { lat: 40.150032, lon: -111.862434 },
+    VT: { lat: 44.045876, lon: -72.710686 },
+    VA: { lat: 37.769337, lon: -78.169968 },
+    WA: { lat: 47.400902, lon: -121.490494 },
+    WV: { lat: 38.491226, lon: -80.954032 },
+    WI: { lat: 44.268543, lon: -89.616508 },
+    WY: { lat: 42.755966, lon: -107.302490 },
+  };
 
 
   const cities = [...new Set(pointData.map((point) => point.city))];
@@ -92,6 +145,7 @@ export default function PointMap() {
       hawaiiMap.setView([21.3114, -157.7964], 5);
     }
   };
+  
 
   useEffect(() => {
     const updateMapWithFilteredData = (validData) => {
@@ -125,20 +179,39 @@ export default function PointMap() {
           { icon: customIcon }
         ).addTo(pointLayerRef.current);
         
-        // Modify your popup code
-        marker.bindPopup(`
-        <div class="popup-content">
-    <img src="${point.url}" alt="${point.first_name} ${point.last_name}" style="width: 100px; height: 110px;"><br>
-    <strong>${point.first_name} ${point.last_name}</strong><br>
-    Location: ${point.city}, ${point.state}<br>
-    Incident Date: ${point.incident_date}<br>
-    Gender: ${point.gender}<br>
-    Age: ${point.age}<br>
-    <div class="popup-bio">
+        let popUpContent = `<div class="popup-content">`;
+    
+    if (point.url) {
+      popUpContent += `
+        <img src="${point.url}" alt="${point.first_name} ${point.last_name}" style="width: 100px; height: 110px;"><br>`;
+    }
+
+    popUpContent += `
+      <strong>${point.first_name} ${point.last_name}</strong><br>
+      Location: ${point.city}, ${point.state}<br>
+      Incident Date: ${point.incident_date}<br>
+      Gender: ${point.gender}<br>
+      Age: ${point.age}<br>
+      <div class="popup-bio">
         Description: ${point.bio_info}
-    </div>
-</div>
-        `);
+      </div>
+    </div>`;
+
+    marker.bindPopup(popUpContent);
+
+    if (point.state === "AK") {
+      const alaskaMarker = L.marker(
+        [point.latitude, point.longitude],
+        { icon: customIcon }
+      ).addTo(alaskaMapRef.current);
+    }
+
+    if (point.state === "HI") {
+      const hawaiiMarker = L.marker(
+        [point.latitude, point.longitude],
+        { icon: customIcon }
+      ).addTo(hawaiiMapRef.current);
+    }
       });
     };
 
@@ -192,14 +265,24 @@ export default function PointMap() {
           });
           hawaiiBasemapLayer.addTo(hawaiiMap);
         }
-
-
         updateMapWithFilteredData(validData);
       } catch (error) {
         console.error("Error initializing main map:", error);
       }
     };
 
+    if (selectedState !== "All") {
+      const state = stateCoordinates[selectedState];
+      if (state) {
+        map.setView([stateCoordinates[selectedState].lat, stateCoordinates[selectedState].lon], 6);
+      }
+    }
+
+    if (map) { 
+      if (selectedState === "All") {
+        map.setView([40.0902, -100.7129], 5);
+      }
+    }
     initializeMap();
   }, [selectedState, selectedGender, selectedAgeRange]);
 
